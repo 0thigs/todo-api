632031f5c2a8ba31012d10e09e4f6a8f7e3f3bd8
0thigs <machinimavlogs@gmail.com>
2024-07-29 17:24:39 -0300

    🖥script: add routes and params

---
+++ server.js
@@ -0,0 +1,38 @@
+import { fastify } from "fastify";
+import { databasePostgres } from "./database-postgres.js";
+
+const server = fastify()
+
+const database = new databasePostgres()
+
+//VISUALIZAR TODOS✅
+server.get('/todos', async (request, response) => {
+    const todos = await database.list()
+    console.log(todos)
+    return todos
+})
+
+//POSTAR TODOS
+server.post('/todos', async (request, response) => {
+    const { todo, done } = request.body
+    await database.post({
+        todo,
+        done
+    })
+    return response.status(204).send()
+})
+
+//DELETAR TODOS
+server.delete('/todos', async (request, response) => {
+    const { id } = request.body
+    await database.delete(id)
+})
+
+//ALTERAR TODOS
+server.put('/todos', (request, response) => {
+    
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
